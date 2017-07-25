21f2110709d5a0d98d61c3d4039ecd7ee45b478e
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-07-25 14:24:06 -0700

    started edit code

---
+++ app/javascript/packs/edit_checklist.js
@@ -0,0 +1,24 @@
+import Vue from 'vue/dist/vue.js'
+import App from './app.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+document.addEventListener('DOMContentLoaded', () => {
+  Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+
+  var app = new Vue({
+    // TODO
+    // updateChecklist() {
+    //   console.log("Update checklist");
+    //   this.$http.put('/checklists/:id', { checklist: this.$data })
+    //     .then(successResponse => {
+    //       // TODO: Redirect to checklist's show page
+    //       console.log(successResponse.body._id.$oid)
+    //       window.location = "/checklists/" + successResponse.body._id.$oid
+    //     }, errorResponse => {
+    //       // TODO: Handle error
+    //     })
+    // }
+  })
+})
