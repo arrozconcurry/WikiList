7fb0db3310791d788d4879f7fd32bda556553b0f
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-07-24 22:55:09 -0700

    internal sweetalert files not referenced properly. went with cdn for now. trying to target checkbox, js is changing all of them

--- app/javascript/packs/show_checklist.js
+++ app/javascript/packs/show_checklist.js
@@ -3,14 +3,10 @@ import App from './app.vue'
 import sweetalert from 'sweetalert';
 
 document.addEventListener('DOMContentLoaded', () => {
-  var checkmark = new Vue({
-    el: '#checking',
-    methods: {
-      animationMessage() {
-        if(allChecked) {
-          sweetalert('Success!', 'Checklist completed!', 'success');
-        }
-      }
+  var cked = new Vue({
+    el: '#mark',
+    data: {
+      checked: false
     }
-  }
+  })
 })
