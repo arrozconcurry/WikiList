c8bad5aafc5eeb981fe6641a9d725d56259a44c4
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-07-26 17:03:31 -0700

    flashing colors added to konami

--- app/javascript/packs/edit_checklist.js
+++ app/javascript/packs/edit_checklist.js
@@ -1,27 +1,47 @@
 import Vue from 'vue/dist/vue.js'
 import App from './app.vue'
 import KonamiCode from 'vue-konami-code'
+import sweetalert from 'sweetalert'
 // import VueResource from 'vue-resource'
 
 // Vue.use(VueResource)
 Vue.use(KonamiCode, {callback: function() {
-  alert('Snake? Snake!? Snaaaaake!')
+  swal({
+    title: "Konami Powers Ignite!",
+    text: "It's a party!",
+    // timer: 5000,
+    imageUrl: "http://cultofthepartyparrot.com/parrots/hd/discoparrot.gif"
+  },
+  function() {
+    var s = document.getElementById('colors').style,
+    f = false,
+    c1 = '#d78cff',
+    c2 = '#88ff89',
+    c3 = '#ff8cff',
+    c4 = '#8bb5fe';
+
+    setInterval(function() {
+      s.backgroundColor = f ? c2 : c1;
+      s.color = f ? c3 : c4;
+      f = !f;
+    }, 500);
+  }());
 }})
 
 // document.addEventListener('DOMContentLoaded', () => {
-  // Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-  // var app = new Vue({
-    // TODO
-    // updateChecklist() {
-    //   console.log("Update checklist");
-    //   this.$http.put('/checklists/:id', { checklist: this.$data })
-    //     .then(successResponse => {
-    //       // TODO: Redirect to checklist's show page
-    //       console.log(successResponse.body._id.$oid)
-    //       window.location = "/checklists/" + successResponse.body._id.$oid
-    //     }, errorResponse => {
-    //       // TODO: Handle error
-    //     })
-    // }
-  // })
+// Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+// var app = new Vue({
+// TODO
+// updateChecklist() {
+//   console.log("Update checklist");
+//   this.$http.put('/checklists/:id', { checklist: this.$data })
+//     .then(successResponse => {
+//       // TODO: Redirect to checklist's show page
+//       console.log(successResponse.body._id.$oid)
+//       window.location = "/checklists/" + successResponse.body._id.$oid
+//     }, errorResponse => {
+//       // TODO: Handle error
+//     })
+// }
+// })
 // })
